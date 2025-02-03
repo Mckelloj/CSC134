738ebed65335705d0c5712840eac6fdc773f3680
Mckelloj <mckelloj0829@student.faytechcc.edu>
2025-02-03 14:43:52 -0500

    Add files via upload

---
+++ PythonMe.py
@@ -0,0 +1,49 @@
+from turtle import *
+t = Turtle()
+t.pensize(3)
+t.pencolor("red")
+t.speed(3)
+
+t.forward(100)
+t.left (90)
+t.forward(100)
+t.left (90)
+t.forward(100)
+t.left (90)
+t.forward(100)
+t.left (90)
+
+t.forward(35)
+t.right(90)
+t.forward(30)
+t.right(90)
+        
+t.forward(50)
+t.left (90)
+t.forward(120)
+t.left (90)
+        
+t.forward(20)
+t.left (90)
+t.forward(90)
+t.right(90)
+        
+t.forward(10)
+t.right(90)
+t.forward(200)
+t.left(90)
+
+t.forward(20)
+t.left(90)
+t.forward(100)
+t.right(90)
+
+t.forward(30)
+t.right(90)
+t.forward(90)  
+t.left(90)
+
+t.left(90)
+t.forward(20)
+t.left(90)
+t.forward(100)
